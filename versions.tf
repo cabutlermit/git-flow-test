45f4c9cda1f09df98d0aac95c7f32863f81cc4d8
Christopher Butler <cabutler@mit.edu>
2024-02-22 16:19:46 -0500

    WIP: Test fmt
    Introduces some non-standard spacing that should get caught by
    the `terraform fmt` hook.

--- versions.tf
+++ versions.tf
@@ -2,6 +2,7 @@
 # All version requirements for Terraform and Terraform Providers are set here.
 # Providers themselves are set in the `providers.tf` file.
 
+# an edit to verify hooks
 terraform {
-  required_version = "~> 1.2"
+  required_version    = "~> 1.2"
 }

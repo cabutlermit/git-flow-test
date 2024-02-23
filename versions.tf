c16efe9ad5963c518a7fcdbd88ce5de09b2dfd97
Christopher Butler <60672715+cabutlermit@users.noreply.github.com>
2024-02-23 09:08:02 -0500

    Testing Squash Commits (#23)
    * Skipped local pre-commit
    
    Why these changes are being introduced:
    This fails pre-commit locally, so it was commited with `--no-verify` so
    I could test what happens in GHA with pre-commit failures.
    
    * WIP: test
    
    * Ensure modules folder exists
    
    * Clean up for pre-commit
    
    * WIP: just some documentation
    
    * WIP: More README changes
    
    * WIP: Test fmt
    
    Introduces some non-standard spacing that should get caught by
    the `terraform fmt` hook.
    
    * WIP: Fix formatting
    
    * WIP: Another fmt check

---
+++ versions.tf
@@ -0,0 +1,8 @@
+################################################################################
+# All version requirements for Terraform and Terraform Providers are set here.
+# Providers themselves are set in the `providers.tf` file.
+
+# an edit to verify hooks
+terraform {
+  required_version   = "~> 1.2"
+}
